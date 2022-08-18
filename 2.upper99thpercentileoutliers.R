be08fd69cd04f3e7c1309b4546ca8d20484b5a72
stymwg <94900556+stymwg@users.noreply.github.com>
2022-08-18 11:29:58 +0100

    Add files via upload

---
+++ 2.upper99thpercentileoutliers.R
@@ -0,0 +1,16 @@
+##By Matthew Gaskins. Last edited 17/08/22.
+setwd("D:/") #sets working directory containing list of windows 
+all <- read_excel("allscaffolds.xlsx") #Manual Input - requires VCF of biallelic SNPs
+new_df <- subset(all, Fst_WC<1 & Fst_WC>0) #subsets all FST values between 0 and 1
+new_df2 <- subset(new_df, snp_count>25) #extracts all windows with more than 25 SNPs
+hist(new_df2$Fst_WC) #plots histogram
+d <- density(new_df2$Fst_WC)
+plot(d) #plots frequency distribution
+
+
+cutoff <- round(nrow(new_df2)*0.01) #cutoff for 1% outliers - can be changed depending on desired outlier number
+library(dplyr)
+arrangedFST <- arrange(new_df2,desc(Fst_WC)) #arranges FST_WC from largest to smallest
+FSToutliers_all <- arrangedFST[1:cutoff,]
+ggplot(FSToutliers_all, aes(x = window_start, y = Fst_WC, fill = scaffold)) + geom_point(size=2, shape=23) #plots location of identified outliers
+FSToutliers <- arrangedFST[1:cutoff,] #extracts 1% outliers
